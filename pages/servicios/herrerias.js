f05ba812641430496c6d6d41412506c53b670645
Lucas Ferreyra <lucasferreyra10@gmail.com>
2025-09-03 19:38:04 -0300

    Services changes v3

---
+++ pages/servicios/herrerias.js
@@ -0,0 +1,13 @@
+// pages/servicios/herrerias.js
+import Layout from "../../components/Layout";
+
+export default function Herrerias() {
+  return (
+    <Layout>
+      <h1 className="text-2xl font-bold mb-4">Herrerías</h1>
+      <p>
+        Aquí irá el contenido de la sección de <strong>Herrerías</strong>.
+      </p>
+    </Layout>
+  );
+}
